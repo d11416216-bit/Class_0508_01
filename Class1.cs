087be777dc68bb4d13cd3b6e9c0cc20d15350534
d11416216-bit <d11416216@gs.takming.edu.tw>
2026-05-08 11:08:33 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Person 
+{
+
+    public virtual string GetName()
+    {
+        return "Person";
+    }
+}
+
+public class Student : Person
+{
+
+    public override string GetName()
+    {
+        return "Student";
+    }
+}
+
+public class People
+{
+
+    public string GetName()
+    {
+        return "People";
+    }
+}
+
+public class Teacher : People
+{
+
+    public string GetName()
+    {
+        return "Teacher";
+    }
+}
